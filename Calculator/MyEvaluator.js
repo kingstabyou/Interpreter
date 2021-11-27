8304932265bbce745213450cb1769b1ff2bb0f4f
Kinstabyou <rahman.maazur@gmail.com>
2021-11-26 20:20:36 -0600

    commented

--- Calculator/MyEvaluator.js
+++ Calculator/MyEvaluator.js
@@ -2,16 +2,11 @@ var result = function(parsedTree){
     return Evaluator(parsedTree);
 }
 
+let variables={}
 function Evaluator(parsedTree){
-    if(parsedTree.type==="number") return parsedTree.value;
+    if(parsedTree.type=="number") return parsedTree.value;
     else if(binOpEval[parsedTree.type]) return binOpEval[parsedTree.type](Evaluator(parsedTree.left),Evaluator(parsedTree.right));
-
-    // switch(parsedTree.type){
-    //     case "number": return parsedTree.value
-    //     break;
-    //     case binOpEval[parsedTree.type] :
-    //         return binOpEval[parsedTree.type](Evaluator(parsedTree.left),Evaluator(parsedTree.right));
-    // }
+    // else if(parsedTree.type=="assign") variables[parsedTree.name]=Evaluator(parsedTree.value); 
 }
 
 var binOpEval={
